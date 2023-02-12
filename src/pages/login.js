73da3838d8480e66351ce90984eeb74509b7d1b5
Idoko Agbo <idokojudge@gmail.com>
2023-02-12 08:47:33 -0500

    add login screen

---
+++ src/pages/login.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "../stylesheet/loginPage.css";
+
+function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(`Username: ${username} Password: ${password}`);
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-input">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div className="form-input">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
+
