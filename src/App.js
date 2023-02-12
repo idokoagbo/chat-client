9915e6e368e18189de986b878b59c9314e00c49e
Idoko Agbo <idokojudge@gmail.com>
2023-02-12 08:46:41 -0500

    add routing to multiple pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ChatApp from './pages/chat';
+import LoginPage from './pages/login';
+import ChatAgentPage from './pages/chatAgent';
 
 function App() {
   return (
-    <ChatApp />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<ChatApp />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/chat-agent" element={<ChatAgentPage />} />
+      </Routes>
+    </Router>
+
   );
 }
 
