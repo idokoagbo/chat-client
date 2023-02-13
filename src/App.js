a61f75c6e44c5a6276f5d41591c92519c3089545
Idoko Agbo <idokojudge@gmail.com>
2023-02-13 15:06:10 -0500

    add new route for admin and response

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ChatApp from './pages/chat';
 import LoginPage from './pages/login';
 import ChatAgentPage from './pages/chatAgent';
+import AdminPage from './pages/admin';
+import ResponseForm from './pages/response';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Routes>
         <Route exact path="/" element={<ChatApp />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminPage />} />
         <Route path="/chat-agent" element={<ChatAgentPage />} />
+        <Route path="/add-response" element={<ResponseForm />} />
       </Routes>
     </Router>
 
